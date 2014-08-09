d7a11f47422c44b571392afa56f54b10486184ca
Dvid Silva <dsilva@doctorondemand.com>
2014-08-09 01:47:06 -0700

    changing to sidemenu starter

--- www/js/app.js
+++ www/js/app.js
@@ -7,78 +7,71 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('spartan', ['ionic', 'spartan.controllers', 'spartan.services', 'firebase'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('login', {
+            url: "/login",
+            abstract: true,
+            templateUrl: "templates/login.html",
+            controller: 'LoginCtrl'
+        })
+        .state('app', {
+            url: "/app",
+            abstract: true,
+            templateUrl: "templates/menu.html",
+            controller: 'AppCtrl'
+        })
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
+    .state('app.search', {
+        url: "/search",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/search.html"
+            }
+        }
     })
 
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
+    .state('app.browse', {
+        url: "/browse",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/browse.html"
+            }
         }
-      }
     })
+        .state('app.playlists', {
+            url: "/playlists",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/playlists.html",
+                    controller: 'PlaylistsCtrl'
+                }
+            }
+        })
 
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
+    .state('app.single', {
+        url: "/playlists/:playlistId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/playlist.html",
+                controller: 'PlaylistCtrl'
+            }
         }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
-        }
-      }
     });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
 });
-
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,14 @@
 angular.module('spartan.controllers', [])
     .controller('mainCtrl', function ($scope) {
+        var already;
         $scope.user = {
             auth: false,
             data: {}
         };
+        already = localStorage['data.user'];
+        if(already !== void 0 && already !== ''){
+            $scope.user = JSON.parse(already);
+        }
     })
     .controller('DashCtrl', function ($scope) {})
 
@@ -16,12 +21,14 @@ angular.module('spartan.controllers', [])
 })
 
 .controller('AccountCtrl', function ($scope, $firebase, $firebaseSimpleLogin) {
-    var ref = new Firebase("https://mtolympus.firebaseio.com/");
-    var auth = $firebaseSimpleLogin(ref);
+    var ref, auth;
+    ref = new Firebase("https://mtolympus.firebaseio.com/");
+    auth = $firebaseSimpleLogin(ref);
 
     $scope.loginWithFacebook = function () {
         auth.$login("facebook").then(function (user) {
             $scope.user.auth = true;
+            user.picture = user.thirdPartyUserData.picture.data.url
             angular.extend($scope.user, {
                 data: user
             });
@@ -30,13 +37,13 @@ angular.module('spartan.controllers', [])
                 provider: user.provider,
                 provider_id: user.id,
                 details: user.thirdPartyUserData, 
-                picture: user.thirdPartyUserData.picture.data.url
+                picture: user.picture
             });
-            console.log("Logged in as: " + user.uid);
+            localStorage['data.user'] = JSON.stringify(angular.copy($scope.user));
         }, function (error) {
             $scope.user.auth = false;
             $scope.user.data = {};
-            console.error("Login failed: " + error);
+            localStorage['data.user'] = '';
         });
     };
 
@@ -45,13 +52,60 @@ angular.module('spartan.controllers', [])
         auth.$logout();
         $scope.user.auth = false;
         $scope.user.data = {};
+        localStorage['data.user'] = '';
     };
 
     // create an AngularFire reference to the data
-    var sync = $firebase(ref);
+    // var sync = $firebase(ref);
     // syncObject.$bindTo($scope, "data");
 
     // download the data into a local object
-    $scope.user.data = sync.$asObject();
+    // $scope.user.data = sync.$asObject();
 
+})
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
 });
